17d07c1112bdacf369046e37bae8784b1c99153e
sunjiaqi <2412772489@qq.com>
2020-05-26 14:12:11 +0800

    cms后台框架

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    baseUrl: './',
+    assetsDir: 'static',
+    productionSourceMap: false,
+    // devServer: {
+    //     proxy: {
+    //         '/api':{
+    //             target:'http://',
+    //             changeOrigin:true,
+    //             pathRewrite:{
+    //                 '/api':''
+    //             }
+    //         }
+    //     }
+    // }
+}
